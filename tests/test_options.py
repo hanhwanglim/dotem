16dc57c25a67280fb8abba6ef6239af0d1803835
Han Hwang Lim <hanhwanglim@gmail.com>
2024-01-08 20:32:08 +0000

    Fix test

--- tests/test_options.py
+++ tests/test_options.py
@@ -7,4 +7,4 @@ runner = CliRunner()
 def test_version():
     result = runner.invoke(app, ["--version"])
     assert result.exit_code == 0
-    assert result.output == "0.1.0\n"
+    assert result.output == "0.1.1\n"
