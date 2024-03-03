c6df7765af10b524188250acedf5eea4cbbd9214
Han Hwang Lim <hanhwanglim@gmail.com>
2024-03-03 11:57:34 +0000

    Remove all python files

--- hook.sh
+++
@@ -1,9 +0,0 @@
-dotem() {
-  result=$("dotem-cli" "$@")
-
-  if [[ $result == "export "* || $result == "unset "* ]]; then
-    eval "$result"
-  else
-    echo "$result"
-  fi
-}
--- tests/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-TEST_DATA = Path(__file__).parent
