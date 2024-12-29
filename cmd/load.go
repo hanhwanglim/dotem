ad849b11334aa3ca6bc5c58750be6c6955af92aa
Han Hwang Lim <hanhwanglim@gmail.com>
2024-12-29 20:25:54 +0000

    Load command

---
+++ cmd/load.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"maps"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/spf13/cobra"
+)
+
+type Config map[string]any
+
+var configPath string
+
+var loadCmd = &cobra.Command{
+	Use:   "load [profile]",
+	Short: "Loads the environment profile (or default)",
+	Args:  cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		profile := "default"
+		if len(args) > 0 {
+			profile = args[0]
+		}
+
+		var config Config
+
+		if _, err := toml.DecodeFile(configPath, &config); err != nil {
+			log.Fatalf("Error reading TOML file: %v\n", err)
+		}
+
+		parts := strings.Split(profile, ".")
+		envVars := resolveEnvVars(config, parts)
+		for key, value := range envVars {
+			fmt.Printf("export %s=\"%v\"\n", key, value)
+		}
+	},
+}
+
+func resolveEnvVars(config Config, profile []string) map[string]any {
+	envVars := make(map[string]any)
+	tables := make([]map[string]any, 0)
+
+	for key, value := range config {
+		if _, isTable := value.(map[string]any); !isTable {
+			envVars[key] = value
+			continue
+		}
+
+		if len(profile) > 0 && key == profile[0] {
+			tables = append(tables, value.(map[string]any))
+		}
+	}
+
+	// Keys in the subtable should take precedence over the ones in the main table.
+	for _, value := range tables {
+		maps.Copy(envVars, resolveEnvVars(value, profile[1:]))
+	}
+
+	return envVars
+}
+
+func init() {
+	rootCmd.AddCommand(loadCmd)
+	loadCmd.Flags().StringVarP(&configPath, "config", "c", "dotem.toml", "Path to the TOML configuration file")
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hanhwanglim/dotem
 go 1.22.3
 
 require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
